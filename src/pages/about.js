18ec886be091e8fe5a3aba53907a9893036bccae
Victor Brugnolo <brugnolovictor@gmail.com>
2020-04-23 13:03:17 -0300

    carregando imagem de perfil

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,13 @@
-import React from "react"
+import React from 'react';
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-
+import Layout from '../components/layout';
+import SEO from '../components/seo';
 
 const AboutPage = () => (
   <Layout>
     <SEO title="About" />
     <h1>About Page</h1>
   </Layout>
-)
+);
 
-export default AboutPage
+export default AboutPage;
