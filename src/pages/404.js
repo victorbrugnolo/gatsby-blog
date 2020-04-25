2cc8b4d84ccfd203289aede5abe2e49307a855ae
Victor Brugnolo <brugnolovictor@gmail.com>
2020-04-25 16:49:01 -0300

    estilizando o layout

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,7 @@
-import React from "react"
+import React from 'react';
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from '../components/Layout';
+import SEO from '../components/seo';
 
 const NotFoundPage = () => (
   <Layout>
@@ -9,6 +9,6 @@ const NotFoundPage = () => (
     <h1>NOT FOUND</h1>
     <p>Ops... não tem nada aqui.</p>
   </Layout>
-)
+);
 
-export default NotFoundPage
+export default NotFoundPage;
