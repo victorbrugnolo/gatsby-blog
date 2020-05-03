82b92e0ae217e4880dd4e3615af1eb775d21b0b8
victorbrugnolo <brugnolovictor@gmail.com>
2020-05-03 00:39:49 -0300

    adicionando estilos bases para o post

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -4,14 +4,31 @@ import { graphql } from 'gatsby';
 import Layout from '../components/Layout';
 import SEO from '../components/seo';
 
+import {
+  MainContent,
+  PostDate,
+  PostDescription,
+  PostHeader,
+  PostTitle,
+} from '../components/Post/styles';
+
 const BlogPost = ({ data }) => {
   const post = data.markdownRemark;
 
   return (
     <Layout>
       <SEO title={post.frontmatter.title} />
-      <h1>Title: {post.frontmatter.title}</h1>
-      <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
+
+      <PostHeader>
+        <PostDate>
+          {post.frontmatter.date} • {post.timeToRead}
+        </PostDate>
+        <PostTitle>{post.frontmatter.title}</PostTitle>
+        <PostDescription>{post.frontmatter.description}</PostDescription>
+      </PostHeader>
+      <MainContent>
+        <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
+      </MainContent>
     </Layout>
   );
 };
@@ -21,6 +38,8 @@ export const query = graphql`
     markdownRemark(fields: { slug: { eq: $slug } }) {
       frontmatter {
         title
+        description
+        date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
       }
       html
     }
