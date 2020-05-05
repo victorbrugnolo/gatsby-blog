b9bbaf82a861d2744a6002f619b27028025d9ff6
victorbrugnolo <brugnolovictor@gmail.com>
2020-05-05 18:20:13 -0300

    implementando troca de tema light/dark

--- src/components/MenuBar/index.js
+++ src/components/MenuBar/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { Home } from 'styled-icons/boxicons-solid';
 import { SearchAlt2 as Search } from 'styled-icons/boxicons-regular';
@@ -13,36 +13,53 @@ import {
   MenuBarLink,
 } from './styles';
 
-const MenuBar = () => (
-  <MenuBarWrapper>
-    <MenuBarGroup>
-      <MenuBarLink to="/" title="Voltar para Home">
-        <MenuBarItem>
-          <Home />
+const MenuBar = () => {
+  const [theme, setTheme] = useState(null);
+
+  const isDarkMode = theme === 'dark';
+
+  useEffect(() => {
+    setTheme(window.__theme);
+    window.__onThemeChange = () => setTheme(window.__theme);
+  }, []);
+
+  return (
+    <MenuBarWrapper>
+      <MenuBarGroup>
+        <MenuBarLink to="/" title="Voltar para Home">
+          <MenuBarItem>
+            <Home />
+          </MenuBarItem>
+        </MenuBarLink>
+
+        <MenuBarLink to="/search/" title="Pesquisar">
+          <MenuBarItem>
+            <Search />
+          </MenuBarItem>
+        </MenuBarLink>
+      </MenuBarGroup>
+
+      <MenuBarGroup>
+        <MenuBarItem
+          title="Mudar o tema"
+          onClick={() => {
+            window.__setPreferredTheme(isDarkMode ? 'light' : 'dark');
+          }}
+          className={theme}
+        >
+          <Light />
         </MenuBarItem>
-      </MenuBarLink>
 
-      <MenuBarLink to="/search/" title="Pesquisar">
-        <MenuBarItem>
-          <Search />
+        <MenuBarItem title="Mudar visualização">
+          <Grid />
         </MenuBarItem>
-      </MenuBarLink>
-    </MenuBarGroup>
 
-    <MenuBarGroup>
-      <MenuBarItem title="Mudar o tema">
-        <Light />
-      </MenuBarItem>
-
-      <MenuBarItem title="Mudar visualização">
-        <Grid />
-      </MenuBarItem>
-
-      <MenuBarItem title="Ir para o topo">
-        <Arrow />
-      </MenuBarItem>
-    </MenuBarGroup>
-  </MenuBarWrapper>
-);
+        <MenuBarItem title="Ir para o topo">
+          <Arrow />
+        </MenuBarItem>
+      </MenuBarGroup>
+    </MenuBarWrapper>
+  );
+};
 
 export default MenuBar;
