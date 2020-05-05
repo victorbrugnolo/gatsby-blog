9ab7da0cbede1d5968f0bc0bca65f0435088124d
victorbrugnolo <brugnolovictor@gmail.com>
2020-05-04 22:49:42 -0300

    estilizando input de busca

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import algoliasearch from 'algoliasearch/lite';
-import { InstantSearch, SearchBox, Hits } from 'react-instantsearch-dom';
+import { InstantSearch, SearchBox, Hits, Stats } from 'react-instantsearch-dom';
+
+import { SearchWrapper } from './styles';
 
 const algolia = {
   appId: process.env.GATSBY_ALGOLIA_APP_ID,
@@ -11,10 +13,19 @@ const algolia = {
 const searchClient = algoliasearch(algolia.appId, algolia.searchOnlyApiKey);
 
 const Search = () => (
-  <InstantSearch searchClient={searchClient} indexName={algolia.indexName}>
-    <SearchBox />
-    <Hits />
-  </InstantSearch>
+  <SearchWrapper>
+    <InstantSearch searchClient={searchClient} indexName={algolia.indexName}>
+      <SearchBox autoFocus translations={{ placeholder: 'Pesquisar...' }} />
+      <Stats
+        translations={{
+          stats(nbHits, timeSpentMs) {
+            return `${nbHits} resultados encontrados em ${timeSpentMs}ms`;
+          },
+        }}
+      />
+      <Hits />
+    </InstantSearch>
+  </SearchWrapper>
 );
 
 export default Search;
